8cc39f1eda29ad3b1b299f1387e6cdb103772a25
wugang <409379158@qq.com>
2023-03-30 23:11:04 +0800

    项目初始化

---
+++ common/src/components/Film.vue
@@ -0,0 +1,35 @@
+<template>
+  <div >
+
+    <ul>
+      <li v-for="data in dataList" :key="data" @click="handchang(data)">
+         {{data}}
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Film',
+  data () {
+    return {
+      dataList: []
+    }
+  },
+  mounted () {
+    axios({
+      url: 'https://m.maizuo.com/gateway?k=479750',
+      method: 'GET',
+      Headers: {
+        'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.2.1","e":"16800149252185451158896641"}',
+        'X-Host': 'mall.film-ticket.city.list'
+      }
+    }).then(res => {
+      this.dataList = res.data.data.films
+    })
+  }
+}
+</script>
